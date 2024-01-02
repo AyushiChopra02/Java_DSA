ecb5a1580f4bd0f246a979c8bb925cf7cb9aeb74
Ayushi <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-02 23:28:31 +0530

    Update GCD.java

--- DSA_Java/src/lec3/GCD.java
+++ DSA_Java/src/lec3/GCD.java
@@ -4,7 +4,7 @@ public class GCD {
 public static void main(String[] args) {
 	int n1 =26 ;
 	int n2 = 74;
-	int divisor = n1 ;
+	int divisor = n1;
 	int dividend = n2;
 	int rem = 0;
 	while(rem> 0) {

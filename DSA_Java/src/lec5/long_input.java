084ee50054f67aa91b47f50c286bf0be89d77fdf
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-03 23:27:16 +0530

    add

--- DSA_Java/src/lec5/long_input.java
+++ DSA_Java/src/lec5/long_input.java
@@ -6,7 +6,7 @@ public class long_input {
 	public static void main(String[] args) {
 		
 	
-Scanner scn = new Scanner(System.in);
+Scanner scn = new Scanner(System.in) ;
 long l = scn.nextLong();
 System.out.println(l+20);
 

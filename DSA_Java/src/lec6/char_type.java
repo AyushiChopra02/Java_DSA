308861c9aea136f7078675ddf6a57fa90745539b
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-04 20:49:25 +0530

    add

---
+++ DSA_Java/src/lec6/char_type.java
@@ -0,0 +1,12 @@
+package lec6;
+
+public class char_type {
+public static void main(String[] args) {
+	char ch = 'a';
+	System.out.println((int)ch);
+	System.out.println((char)('a'+25));
+	
+	char cha ='$';
+	System.out.println((int)cha);
+}
+}

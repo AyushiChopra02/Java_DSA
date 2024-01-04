2b522221e3cc8def3e4f3015ab568e15a1545dc4
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-04 21:34:18 +0530

    add

--- DSA_Java/src/lec6/char_type.java
+++ DSA_Java/src/lec6/char_type.java
@@ -2,7 +2,7 @@ package lec6;
 
 public class char_type {
 public static void main(String[] args) {
-	char ch = 'a';
+	char ch = 'Z';
 	System.out.println((int)ch);
 	System.out.println((char)('a'+25));
 	
---
+++ DSA_Java/src/lec6/lower_upper.java
@@ -0,0 +1,15 @@
+package lec6;
+
+public class lower_upper {
+public static void main(String[] args) {
+	char ch = 'a';
+	if(ch >= 'a' && ch <= 'z') {
+		System.out.println("chota");
+	}else if(ch >= 'A' && ch<= 'Z'){
+		System.out.println("budha");
+		
+	}else {
+		System.out.println("nai aata");
+	}
+}
+}

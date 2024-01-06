a604a5a354d0a66798b0784461dca8373d722a0f
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-07 01:56:54 +0530

    add

---
+++ DSA_Java/src/lec6/ShoppingGame.java
@@ -0,0 +1,43 @@
+package lec6;
+
+import java.util.Scanner;
+
+public class ShoppingGame {
+	
+	
+	    public static void main(String[] args) {
+	        Scanner scn = new Scanner(System.in);
+
+	        System.out.print(" max smartphones Aayush can purchase (M): ");
+	        int m = scn.nextInt();
+
+	        System.out.print("max smartphones Harshit can purchase (N): ");
+	        int n = scn.nextInt();
+
+	        int aayushRemaining = m;
+	        int harshitRemaining = n;
+
+	        
+	        while (true) {
+	            
+	            for (int i = 1; i <= aayushRemaining; i++) {
+	                aayushRemaining -= i;
+	                if (aayushRemaining <= 0) {
+	                    System.out.println("Harshit");
+	                    return;
+	                }
+	            }
+
+	           
+	            for (int i = 1; i <= harshitRemaining; i++) {
+	                harshitRemaining -= i;
+	                if (harshitRemaining <= 0) {
+	                    System.out.println("Aayush");
+	                    return;
+	                }
+	            }
+	        }
+	    }
+}	
+
+

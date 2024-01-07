b13330a3c49e42181f7ae3c63115af0ea133dbe9
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-08 02:53:40 +0530

    add

---
+++ DSA_Java/src/lec7/fnctn.java
@@ -0,0 +1,18 @@
+package lec7;
+
+public class fnctn {
+public static void main(String[] args) {
+	party();
+	int ans = add(10,15,-7);
+	System.out.println();
+}
+public static void party() {
+	System.out.println("naacho");
+}
+//hm chahhte h add int return kre mtlb return typeint h isliye void ki jgah int likha
+public static int add(int a , int b,int c){
+	System.out.println(a+b+c);
+	return(a+b+c);
+	
+}
+}

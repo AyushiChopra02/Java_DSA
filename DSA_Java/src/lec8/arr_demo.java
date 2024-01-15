081270463d75b42001f79c2f0cac7e366f5a124e
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-15 23:58:23 +0530

    add

--- DSA_Java/src/lec8/arr_demo.java
+++ DSA_Java/src/lec8/arr_demo.java
@@ -2,7 +2,19 @@ package lec8;
 
 public class arr_demo {
 public static void main(String[] args) {
-	int arr[] = new int[5];
-	System.out.println(arr);
+	//arr ko aise dfine krte h
+	//int arr[] = new int[5];
+	//System.out.println(arr);
+	
+	
+	int arr[] = new int[10];
+	int table = 3;
+	for (int indx =0 ; indx < arr.length ; indx++ ) {
+		arr[indx] = table;
+		table = table+ 3;
+	}
+	for (int indx =0 ; indx < arr.length ; indx++ ) {
+		System.out.println(arr[indx]);
+	}
 }
 }

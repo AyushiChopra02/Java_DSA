7488c5cbc814b1265f61ef25054673ab04491956
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-18 16:21:46 +0530

    add

---
+++ DSA_Java/src/lec10/Subset.java
@@ -0,0 +1,15 @@
+package lec10;
+
+public class Subset {
+public static void main(String[] args) {
+	int arr[] = {10,20,30,40,50};
+	for(int s =0 ; s < arr.length ; s++) {
+		for(int e = s ; e<arr.length ; e++) {
+			for(int i=s; i<=e ; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
+}

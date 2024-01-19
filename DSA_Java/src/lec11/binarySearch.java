ebdcd32b1f63b0d92863e5a17ec8c9cda4c81418
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-19 23:35:09 +0530

    add

--- DSA_Java/src/lec11/binarySearch.java
+++ DSA_Java/src/lec11/binarySearch.java
@@ -11,11 +11,13 @@ while(strt <= end) {
 		end = mid - 1;
 		
 	}
-	else if(arr[mid]<elmnt){
+	else if(arr[mid]<elmnt ){
 		strt = mid + 1;
 	}
 	else {
 		System.out.println("elemnt found at index" + mid);
 	}
+	System.out.println("elemnt not found");
+}
 }
 }

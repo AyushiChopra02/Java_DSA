0d64a7289bf73dffad99bee52e87a96f59800436
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-21 01:32:52 +0530

    add

---
+++ DSA_Java/src/lec12/selcnSorting.java
@@ -0,0 +1,26 @@
+package lec12;
+
+import java.util.Arrays;
+
+public class selcnSorting {
+public static void main(String[] args) {
+	int arr[] = {50,40,30,20,10};
+	System.out.println(Arrays.toString(arr));
+	Selection(arr);
+	System.out.println(Arrays.toString(arr));
+}
+public static void Selection(int arr[]) {
+	for(int last = arr.length - 1; last >=1; last--) {
+		int max_id =0;
+		for(int i=0 ; i <=last ; i++) {
+			if (arr[i] > arr[max_id]) {
+				max_id =i;
+			}
+		}
+		//swap
+		int temp = arr[last];
+		arr[last]= arr[max_id];
+		arr[max_id] =temp;
+	}
+}
+}

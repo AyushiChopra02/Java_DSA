e1f15cfd4d3a81e2b6cc663fae431d33ede487e3
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-23 01:11:05 +0530

    add

---
+++ DSA_Java/src/lec13/arr_2D.java
@@ -0,0 +1,27 @@
+package lec13;
+
+public class arr_2D {
+public static void main(String[] args) {
+	int mat [] [] = new int[5][3];
+	System.out.println(mat);
+	System.out.println(mat.length);
+	
+	for(int[] row : mat) {
+		for(int ali :row) {
+			System.out.println(ali + " ");
+			
+		}
+		System.out.println();
+	}
+	System.out.println("=========");
+	mat[0][0] = 1;
+	mat[0][1] = 2;
+	mat [0][2] = 3;
+	for( int row[] : mat) {
+		for(int ali : row) {
+			System.out.println(ali + " ");
+		}
+		System.out.println();
+	}
+}
+}

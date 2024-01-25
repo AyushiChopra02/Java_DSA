bbee450fae0346dd7d4142c954575259b4e7f9f4
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-25 23:28:19 +0530

    add

--- DSA_Java/src/lec8/MaxInArray.java
+++ DSA_Java/src/lec8/MaxInArray.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MaxInArray {
 	
 	    public static void main(String args[]) {
-	        Scanner scn = new Scanner(System.in);
+	        Scanner scn = new Scanner(System.in) ;
 	        int n = scn.nextInt();
 	        int arr[] = new int[n];
 	        

c813c2ffde676c42ba3bd17cb555c5a35a3f4fe6
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-29 18:22:37 +0530

    add

--- DSA_Java/src/lec14/palindrome.java
+++ DSA_Java/src/lec14/palindrome.java
@@ -4,6 +4,7 @@ public class palindrome {
 	public static void main(String[] args) {
 		String str = "mom";
 	System.out.println(isPalindrome(str));
+	printAllpalinSS2("nnittin");
 }
 	public static boolean isPalindrome(String str) {
 		int s =0;
@@ -16,7 +17,27 @@ public class palindrome {
 			s++; e--;
 		}
 		return true;
-		
-		
+		}
+	
+	
+	
+	
+	//saare palindrome print krne k lie
+	public static void printAllpalinSS2(String str) {
+		for(int  c=0 ; c<str.length() ; c++) {
+			grow(str,c,c);
+				
+			}
+		}
+	public static void grow(String str, int L, int R) {
+		while(L >= 0 && R<str.length()) {
+			if(str.charAt(L) == str.charAt(R)) {
+				System.out.println(str.substring(L , R+1));
+				L--;
+				R++;
+			}else {
+				break;
+			}
+	}
 	}
 }

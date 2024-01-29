e079b36544678b724b3b02e6e1360d8f3f0e55eb
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-29 17:11:38 +0530

    add

---
+++ DSA_Java/src/lec14/string_demo.java
@@ -0,0 +1,9 @@
+package lec14;
+
+public class string_demo {
+public static void main(String[] args) {
+	String str = "abcd";
+	System.out.println(str.length());
+	System.out.println(str.charAt(0));
+}
+}

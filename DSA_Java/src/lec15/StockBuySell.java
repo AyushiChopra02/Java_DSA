2612bd3c4fd20783ac420a5aea3cb1a1c35b9c19
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-01-30 18:33:01 +0530

    add

---
+++ DSA_Java/src/lec15/StockBuySell.java
@@ -0,0 +1,17 @@
+package lec15;
+
+public class StockBuySell {
+	
+	    public int maxProfit(int[] prices) {
+	        int maxProfit =0;
+	        int minsofar = prices[0];
+	        for(int i =0 ; i<prices.length ; i++){
+	            minsofar = Math.min(minsofar , prices[i]);
+	            int profit = prices[i] - minsofar;
+	            maxProfit = Math.max(maxProfit , profit);
+
+	        }
+	        return maxProfit;
+	    }
+	}
+

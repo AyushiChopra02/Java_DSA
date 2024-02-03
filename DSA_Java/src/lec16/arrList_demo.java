9f8e7fd23016e877761cb2e1549e9c3cdf4c4c78
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-02-04 01:42:40 +0530

    add

---
+++ DSA_Java/src/lec16/arrList_demo.java
@@ -0,0 +1,20 @@
+package lec16;
+
+import java.util.ArrayList;
+
+public class arrList_demo {
+public static void main(String[] args) {
+	ArrayList<Integer> AL = new ArrayList<Integer>();	
+	System.out.println(AL);
+	AL.add(10);
+	System.out.println(AL);
+	//AL size
+	System.out.println(AL.size());
+	AL.add(50);
+	System.out.println(AL);
+	
+	//indexing
+	System.out.println(AL.get(0));
+	System.out.println(AL.get(AL.size()-1));
+}
+}

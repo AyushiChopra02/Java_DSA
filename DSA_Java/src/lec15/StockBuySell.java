d6eeee9fb5852f76713eb3351f66c95e0c36b865
Ayushi <139689074+AyushiChopra02@users.noreply.github.com>
2024-02-08 23:35:05 +0530

    Update StockBuySell.java

--- DSA_Java/src/lec15/StockBuySell.java
+++ DSA_Java/src/lec15/StockBuySell.java
@@ -4,7 +4,7 @@ public class StockBuySell {
 	
 	    public int maxProfit(int[] prices) {
 	        int maxProfit =0;
-	        int minsofar = prices[0];
+	        int minsofar = prices[0] ;
 	        for(int i =0 ; i<prices.length ; i++){
 	            minsofar = Math.min(minsofar , prices[i] );
 	            int profit = prices[i] - minsofar  ;

d12377ba272f26a2bac1685d859f64abe92c99d5
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-02-12 22:16:47 +0530

    add

---
+++ DSA_Java/src/lec20/next_Prmntn.java
@@ -0,0 +1,26 @@
+package lec20;
+
+public class next_Prmntn {
+	public static void main(String[] args) {	
+int num = 981354321;
+char [] arr = (num + "").toCharArray();
+int f= -1;
+char prev = '0';
+for(int idx = arr.length - 1 ; idx >=0 ; idx--) {
+	char curr = arr[idx];
+	if(curr < prev) {
+		f = idx;
+		break;}
+	prev = curr ;
+}
+//swap f with a digit just bigger than arr[f]
+for(int idx = arr.length-1  ; idx > f ;idx--) {
+	if(arr[idx] > arr[f]) {
+		//idx with f swipe krna
+		char temp = arr[idx];
+		arr[idx] = arr[f];
+		arr[f] = temp ;
+		break ; } }
+System.out.println(arr);
+}
+}

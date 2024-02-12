164bd4baddff75d348c2bec2e38575ab8c61abde
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2024-02-12 23:51:43 +0530

    add

---
+++ DSA_Java/src/lec20/RemoveStringDuplc.java
@@ -0,0 +1,26 @@
+package lec20;
+
+import java.util.Scanner;
+
+public class RemoveStringDuplc {
+		public static void main(String[] args) {	    
+			Scanner sc = new Scanner(System.in);
+					String str1 = sc.next();
+					System.out.println(RemoveDuplet(str1));
+				}
+				public static String RemoveDuplet(String input) {
+					if (input.length() <= 1 || input.length() > 1000) {
+						return input;
+					}	
+					if(input.charAt(0)==input.charAt(1)) {
+						return RemoveDuplet(input.substring(1));	
+					}
+					else {
+						return input.charAt(0) + RemoveDuplet(input.substring(1));
+
+					}	 	
+			}
+}
+			
+
+

cac3d8bbe222bee503f78249192c68cd47ca8dbb
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-02-16 18:50:56 +0530

    add

---
+++ DSA_Java/src/lec23/arr_rec.java
@@ -0,0 +1,18 @@
+package lec23;
+
+public class arr_rec {
+public static void main(String[] args) {
+	int arr[] = {10 ,20,30,40};
+	Print(0 , arr);
+}
+public static void Print(int s , int arr[]) {
+	if(s == arr.length) {
+		return ;
+	}
+	//BP : Print(0 , arr)
+	//SP : Print(1,arr)
+	System.out.println(arr[s]);
+	Print(s+1 , arr);
+	
+}
+}

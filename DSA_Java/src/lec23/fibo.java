63af08c59f65f4427b16279b6820adf2963159c1
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-02-17 23:07:55 +0530

    add

---
+++ DSA_Java/src/lec23/fibo.java
@@ -0,0 +1,22 @@
+package lec23;
+
+public class fibo {
+	
+	    public static void main(String[] args) {
+	        int n = 10; 
+	        System.out.println("Fibonacci Series up to " + n + " terms:");
+	        for (int i = 0; i < n; i++) {
+	            System.out.print(fibonacci(i) + " ");
+	        }
+	    }
+
+	    public static int fibonacci(int n) {
+	        if (n <= 1) {
+	            return n;
+	        } else {
+	            return fibonacci(n - 1) + fibonacci(n - 2);
+	        }
+	    }
+	}
+
+

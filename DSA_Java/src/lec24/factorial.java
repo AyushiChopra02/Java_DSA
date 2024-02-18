87a1d58eb2e516f781bacd4740e7a85b680dc16d
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-02-18 22:05:29 +0530

    add

---
+++ DSA_Java/src/lec24/factorial.java
@@ -0,0 +1,17 @@
+package lec24;
+
+public class factorial {
+public static void main(String[] args) {
+	System.out.println(Fac(5));
+}
+public static int Fac(int n) {
+	if(n==0) {
+		return 1 ;
+	}
+	//BP : Fac(n)
+	//SP : FAC(n-1)
+	int sp = Fac(n-1);
+	return n * sp;
+}
+}
+ 
---
+++ DSA_Java/src/lec24/frst_occ.java
@@ -0,0 +1,36 @@
+package lec24;
+public class frst_occ {
+public static void main(String[] args) {
+	int arr[] = { 6 ,5 , 6 , 5 ,5};
+	int firstIndex = FirstOcc(0, arr, 5);
+    System.out.println("First occurrence index: " + firstIndex);
+
+    int lastIndex = LastOcc(0, arr, 5);
+    System.out.println("Last occurrence index: " + lastIndex);}
+public static int FirstOcc(int s , int arr[] , int ali) {
+	if(s== arr.length) {
+		return -1 ;
+	}
+		//BP : FO(s , ali , arr)
+		//SP : FO(s+1 , arr,ali)
+		int sp = FirstOcc(s+1 , arr , ali);
+		if(arr[s] == ali) {
+			return s;
+		}
+		return sp;
+	}
+	public static int LastOcc(int s , int arr[] , int ali) {
+		if (s == arr.length) {
+			return -1 ;
+		}
+		//BP : LO(s , arr , ali)
+		//SP : LO(s+1 , arr , ali)
+		int sp = LastOcc(s+1 , arr , ali);
+			if(sp ==1  && arr[s] == ali) {
+				return s ;
+			}
+			return sp ;
+		}
+	}
+
+

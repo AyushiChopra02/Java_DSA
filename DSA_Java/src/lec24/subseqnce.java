37e798f75dcd1aad58d5673434616d3bcb12b4cd
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-02-20 21:41:25 +0530

    addd

--- DSA_Java/src/lec24/subseqnce.java
+++ DSA_Java/src/lec24/subseqnce.java
@@ -16,7 +16,7 @@ public static void main(String[] args) {
 	                    subsequence.append(str.charAt(j));
 	                }
 	            }
-	            System.out.println(subsequence);
+	            System.out.println(subsequence) ;
 	        }
 	    }}
 

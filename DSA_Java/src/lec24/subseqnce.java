fe60efb7bbd86e5602f20a80bd71fed1c281a95d
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-02-20 20:18:39 +0530

    add

--- DSA_Java/src/lec24/subseqnce.java
+++ DSA_Java/src/lec24/subseqnce.java
@@ -13,7 +13,7 @@ public static void main(String[] args) {
 	            StringBuilder subsequence = new StringBuilder() ;
 	            for (int j = 0; j < n; j++) {
 	                if ((i & (1 << j)) > 0) {
-	                    subsequence.append(str.charAt(j));
+	                    subsequence.append(str.charAt(j)) ;
 	                }
 	            }
 	            System.out.println(subsequence);

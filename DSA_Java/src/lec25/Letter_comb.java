1a0878d096fe5467f74a2aaee8b7074b11df7e0a
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-02-21 21:58:15 +0530

    add

---
+++ DSA_Java/src/lec25/Letter_comb.java
@@ -0,0 +1,26 @@
+package lec25;
+
+public class Letter_comb {
+public static void main(String[] args) {
+	solve("23" , "");
+	
+}
+public static void solve(String buttons , String ans) {
+	//BP : button: 246
+	if(buttons.isEmpty()) {
+		System.out.println(ans);
+		return ;
+		
+	}
+	String remain = buttons.substring(1);//46
+	char ch = buttons.charAt(0); //2
+	String op = options(ch); //abc
+	for(int i=0 ; i<op.length() ; i++) {
+		solve(remain , ans+op.charAt(i));
+	}
+}
+public static String options(char ch) {
+	String[] arr = {"abc" , "def" , "ghi" , "jkl" , "mno" , "pqrs" , "tuv" , "wxyz"};
+	return arr[ch -'2'];
+}
+}
---
+++ DSA_Java/src/lec25/subsqnce.java
@@ -0,0 +1,19 @@
+package lec25;
+
+public class subsqnce {
+public static void main(String[] args) {
+	SS("abc" , "");
+}
+public static void SS(String str , String team) {
+	if(str.isEmpty()) {
+		System.out.println(team +")");
+		return ;
+	}
+	//str = "abcd"
+	//team = "Z"
+	String remain = str.substring(1);
+	char ch = str.charAt(0);
+	SS(remain, team + ch);
+	SS(remain , team);
+}
+}

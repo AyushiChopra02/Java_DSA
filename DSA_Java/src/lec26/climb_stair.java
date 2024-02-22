d997481e698a41fc52e33cc8dcc65f56cb07c7d5
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-02-22 23:28:00 +0530

    add

---
+++ DSA_Java/src/lec26/climb_stair.java
@@ -0,0 +1,19 @@
+package lec26;
+
+public class climb_stair {
+public static void main(String[] args) {
+	kuud(0 , "" , 4);
+}
+public static void kuud(int curr , String path , int dest) {
+	if(curr == dest) { //+ve Base Case
+		System.out.println(path);
+		return ;
+		
+	}
+	if(curr > dest) {
+		return ;
+	}
+	kuud(curr +1 , path+1 , dest);
+	kuud(curr+2 , path+2 , dest);
+}
+}
---
+++ DSA_Java/src/lec26/genrte_parnths.java
@@ -0,0 +1,18 @@
+package lec26;
+
+public class genrte_parnths {
+public static void main(String[] args) {
+	solve(3,3,"");
+}
+public static void solve(int op , int cl , String path) {
+	if(op ==0 && cl ==0) {
+		System.out.println(path);
+		return;
+	}
+	if(op>0) {
+		solve(op-1 , cl , path+ ")");
+		if(cl >0) {
+		solve(op , cl-1 , path + ")");
+	}}
+}
+}

6b0a949c87cc9db58f4cf161ca7a8fb5a54bcd0f
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-02-24 23:59:26 +0530

    add

--- DSA_Java/src/lec27/Predict_the_winner.java
+++ DSA_Java/src/lec27/Predict_the_winner.java
@@ -15,7 +15,7 @@ public class Predict_the_winner {
 	            int j = i + len - 1;
 	//Jab Player 1 nums[i] choose karta hai, to remaining array ka score dp[i + 1][j] se subtract hota hai.
 	//Jab Player 1 nums[j] choose karta hai, to remaining array ka score dp[i][j - 1] se subtract hota hai.
-	            dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+	            dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]) ;
 	        }}    
 	    return dp[0][n - 1] >= 0;
 	    // 0 se (n-1) tak ke elements ka score calculate kiya, agar >= 0 hai to Player 1 jeetega

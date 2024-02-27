d36591ce1e8f0cc91efc4e01d548434cad1a52b5
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-02-27 22:39:06 +0530

    add

---
+++ DSA_Java/src/lec29/comb_all_type.java
@@ -0,0 +1,32 @@
+package lec29;
+
+public class comb_all_type {
+public static void main(String[] args) {
+	int n=4;
+	int r = 2;
+	PT(r,-1 , "",n);
+}
+public static void PT(int r,int prev , String path , int n) {
+	if(r==0) {
+		System.out.println(path);
+		return;
+	}
+	for(int curr=prev +1 ;curr<n ; curr++) {
+		//curr
+		PT(r-1 , curr,path + "b" + curr , n);
+	}
+}
+public static void seat(int curr , int toPlace , String path , int total_seats) {
+	if(toPlace==0) { //+ve basecase
+		System.out.println(path);
+		return;
+	}
+	if(curr==total_seats) { //-ve basecase
+		return ;
+		
+	}
+	seat(curr , toPlace-1 , path+"q"+curr , total_seats);//include
+	seat(curr , toPlace , path +curr , total_seats);//exclude
+	
+}
+}

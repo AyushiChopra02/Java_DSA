e2c8b6dbb37753d3b6db9ea87356afa19f8a4925
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-03-03 23:59:31 +0530

    add

---
+++ DSA_Java/src/lec33/Student.java
@@ -0,0 +1,9 @@
+package lec33;
+
+public class Student {
+
+	//data member
+	String Name;
+	int age;
+}
+
---
+++ DSA_Java/src/lec33/client.java
@@ -0,0 +1,10 @@
+package lec33;
+
+public class client {
+public static void main(String[] args) {
+	Student s1 = new Student();
+	System.out.println(s1);
+	System.out.println(s1.age);
+	System.out.println(s1.Name);
+}
+}

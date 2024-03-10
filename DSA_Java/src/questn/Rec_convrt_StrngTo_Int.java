f379ffb8d91f2ffefc9ade1833f48e1187e2a5bb
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-03-10 20:03:37 +0530

    add

--- DSA_Java/src/questn/Rec_convrt_StrngTo_Int.java
+++ DSA_Java/src/questn/Rec_convrt_StrngTo_Int.java
@@ -4,29 +4,36 @@ import java.util.Scanner;
 
 public class Rec_convrt_StrngTo_Int {
 	
+	    
+	
 	    public static void main(String args[]) {
 	        Scanner scanner = new Scanner(System.in);
-	        System.out.print("Enter a no.: ");
+	        System.out.print("Enter a number in form of a String: ");
 	        String str = scanner.nextLine();
 	        scanner.close();
 
-	        int result = stringToInteger(str) ;
-	        System.out.println("Conv int : " + result);
+	        int result = stringToInteger(str);
+	        System.out.println("Converted Integer: " + result);
 	    }
+
 	    public static int stringToInteger(String str) {
-	        return stringToIntegerHelper(str, str.length() - 1, 0) ;
+	        return stringToIntegerHelper(str, 0); // Recursive helper function call
 	    }
-	    private static int stringToIntegerHelper(String str, int index, int result) {
-	        
-	        if (index < 0) {
-	            return result;
+
+	    private static int stringToIntegerHelper(String str, int index) {
+	        // Base case: if index goes beyond string length, return 0
+	        if (index >= str.length()) {
+	            return 0;
 	        }
-	        int digit = str.charAt(index) - '0' ;
-	        result = result * 10 + digit;
-	        return stringToIntegerHelper(str, index - 1, result);
+
+	       
+	        int digit = str.charAt(index) - '0';
+
+	        
+	        int remaining = stringToIntegerHelper(str, index + 1);
+
+	        
+	        int placeValue = (int) Math.pow(10, str.length() - 1 - index);
+	        return digit * placeValue + remaining;
 	    }
 	}
-
-	    
-
-
--- DSA_Java/src/questn/duplcte_chartr_formtng.java
+++ DSA_Java/src/questn/duplcte_chartr_formtng.java
@@ -4,25 +4,32 @@ import java.util.Scanner;
 
 public class duplcte_chartr_formtng {
 public static void main(String[] args) {
-	
-	    Scanner sc= new Scanner(System.in);
-			        String str1 = sc.next();
-			        System.out.println(duplet(0,1, new StringBuilder(str1)));
-			    }
-			    public static StringBuilder duplet(int i,int j,StringBuilder sb){
-			      if(j==sb.length()){
-
-			          return sb ;
-			      }
-			       if(sb.charAt(i)==sb.charAt(j)){
-			        sb =sb.insert(i+1,'*');
-			       }
-			       return duplet(i+1,j+1,sb) ;
-		}
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter the string: ");
+	        String str = scanner.nextLine();
+	        String result = formatDuplicateCharacters(str);
+	        System.out.println("Updated String: " + result);
+	        scanner.close();
+	    }
+	    public static String formatDuplicateCharacters(String str) {
+	        // Base case: agar string ki length 1 ya usse kam hai, toh wahi return karna hai
+	        if (str.length() <= 1) {
+	            return str;
+	        }
+	        // Dekhte hain ki pehla character agle ke saath match karta hai ya nahi
+	        if (str.charAt(0) == str.charAt(1)) {
+	            // Agar match karta hai, toh '*' add karte hain pehle character ke baad
+	            return str.charAt(0) + "*" + formatDuplicateCharacters(str.substring(1));
+	        } else {
+	            // Agar match nahi karta hai, toh wahi character append karte hain
+	            return str.charAt(0) + formatDuplicateCharacters(str.substring(1));
+	        }
+	    }
+	}
 
 
 	    
 
 
 
-}
+

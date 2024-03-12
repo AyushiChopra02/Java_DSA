f8a10f6f44d0ad8f13fdd7c1f2a961e4cd9cd582
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-03-12 21:43:17 +0530

    add

--- DSA_Java/src/questn/Broadpath.java
+++ DSA_Java/src/questn/Broadpath.java
@@ -12,21 +12,24 @@ public class Broadpath {
         int count = SnakesAndLadders.countPaths(N, M);
         System.out.println(count);
 
-        // Print all the paths
         SnakesAndLadders.printPaths(N, M, "");
     }
 }
-
 class SnakesAndLadders {
-    // Function to count different ways the board can be traveled using the dice
+    // Function to count different ways the board can be travelled using the dice
     static int countPaths(int N, int M) {
-        // Base case: If N is 0, there is only one way to reach the destination
+        // Agar board ka size 0 hai, to ek rasta hi hota hai ki destination tak pahonche.
         if (N == 0)
             return 1;
-
+//path ka count store krega 
         int count = 0;
 
-        // Recursively count paths by rolling the dice
+       // yea loop har ek possible dice value ke liye board par travel karta hai. 
+        //i variable ko 1 se lekar M tak badhaya jata hai, jo dice ke faces ko represent karta hai. Har ek iteration mein,
+        //hum dekhte hain ki current position N se i ko subtract karke naya position kya hai. Agar nayi position valid hai (N - i >= 0), 
+        //matlab ki board ke bahar nahi gaye hain, to hum countPaths function ko us nayi position ke saath phir se call karte hain. 
+        //Isse har ek possible move ke liye hum paths ko count kar sakte hain. Finally, jo bhi count milta hai, 
+        //hum use count variable mein add kar lete hain.
         for (int i = 1; i <= M; i++) {
             if (N - i >= 0) {
                 count += countPaths(N - i, M);
@@ -38,7 +41,7 @@ class SnakesAndLadders {
 
     // Function to print dice-values for all valid paths across the board
     static void printPaths(int N, int M, String path) {
-        // Base case: If N is 0, print the path
+        // Agar board ka size 0 ho jata hai, to hum path ko print karte hain.
         if (N == 0) {
             System.out.print(path + " ");
             return;

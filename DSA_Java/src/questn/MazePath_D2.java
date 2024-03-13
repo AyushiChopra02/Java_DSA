c616b4a03e512cdd1c6ec14f35cb32e8599abdd4
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-03-13 20:11:37 +0530

    add

---
+++ DSA_Java/src/questn/MazePath_D2.java
@@ -0,0 +1,42 @@
+package questn;
+
+import java.util.Scanner;
+
+public class MazePath_D2 {
+	
+	
+
+	
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        int n = scanner.nextInt();
+
+	        int count = findPaths(n, 0, 0, "");
+	        System.out.println(count);
+	    }
+
+	    public static int findPaths(int n, int row, int col, String path) {
+	    	//condtion to chck ki agr hm btm right crnr pr phonche k nai
+	        if (row == n - 1 && col == n - 1) {
+	            System.out.print(path + " ");
+	            return 1;
+	        }
+
+	        int count = 0;
+	      //findPaths function ko niche ki taraf jane ke liye.
+	        if (row < n - 1) {
+	            count += findPaths(n, row + 1, col, path + "V"); // Move down
+	        }
+	        // agar hum right edge tak nahi pahunch gaye hain to.
+	        if (col < n - 1) {
+	            count += findPaths(n, row, col + 1, path + "H"); // Move right
+	        }
+	     // agar hum diagonal move kar sakte hain to.
+	        if ((row == col || row + col == n - 1) && row < n - 1 && col < n - 1) {
+	        	//findPaths function ko diagonal ki taraf jane ke liye.
+	            count += findPaths(n, row + 1, col + 1, path + "D"); // Move diagonally
+	        }
+	     // total paths ka count return karta hai.
+	        return count;
+	    }
+	}
---
+++ DSA_Java/src/questn/TowerOfHanoi.java
@@ -0,0 +1,20 @@
+package questn;
+
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+	public static void towerOfHanoi(int n , char src , char aux , char des) {
+		if(n==1) {
+			System.out.println(src + "->" + des);
+			return ;
+		}
+		towerOfHanoi(n-1 , src , des, aux);
+		towerOfHanoi(1 , src , aux , des);
+		towerOfHanoi(n-1 , aux , src , des);
+	}
+	public static void main(String[] args) {
+		Scanner scr = new Scanner(System.in);
+		int n = scr.nextInt();
+		towerOfHanoi(n , 'A' , 'B' , 'C');
+	}
+}

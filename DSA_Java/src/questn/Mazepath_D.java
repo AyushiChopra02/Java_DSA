7c4b1469dfd9c098903cd506ac13a00ab0408f44
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-03-18 20:21:30 +0530

    //

--- DSA_Java/src/questn/Mazepath_D.java
+++ DSA_Java/src/questn/Mazepath_D.java
@@ -31,7 +31,7 @@ public class Mazepath_D {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in) ;
         int N = sc.nextInt();
         sc.close();

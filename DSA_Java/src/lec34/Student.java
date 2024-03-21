b0439b3f003c0ba44733e5cd510c3a7b908a85ed
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-03-21 20:16:53 +0530

    add

---
+++ DSA_Java/src/lec34/Student.java
@@ -0,0 +1,22 @@
+package lec34;
+
+public class Student {
+String Name ="gun shudaa";
+int Age = 10;
+public void Intro() {
+	System.out.println(Name + "-" + Age);
+	System.out.println(this);
+}
+public void partyWith(String Name) {
+	System.out.println(this.Name + "party with" + Name);
+}
+public Student() {
+	
+}
+public Student(int I) {
+	Age = I ;
+}
+public Student(String str) {
+	Name = str;
+}
+}
---
+++ DSA_Java/src/lec34/client.java
@@ -0,0 +1,11 @@
+package lec34;
+
+public class client {
+public static void main(String[] args) {
+	Student s1 = new Student();
+	System.out.println(s1);
+	System.out.println(s1.Age);
+	
+	System.out.println(s1.Name);
+}
+}

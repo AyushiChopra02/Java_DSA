76bb0e0d948b35c1e4fdaee01bdd4764e75b99fe
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-03-28 03:09:59 +0530

    //

---
+++ DSA_Java/src/lec37/clieant_queue.java
@@ -0,0 +1,15 @@
+package lec37;
+
+public class clieant_queue {
+public static void main(String[] args) {
+	queue Q = new queue();
+	Q.add(10);
+	Q.add(20);
+	Q.add(30);
+	Q.add(40);
+	System.out.println(Q.poll());
+	System.out.println(Q.poll());
+	System.out.println(Q.poll());
+	System.out.println(Q.poll());
+}
+}
---
+++ DSA_Java/src/lec37/queue.java
@@ -0,0 +1,36 @@
+package lec37;
+
+public class queue {
+int arr[];
+int s;
+int size;
+public queue() {
+	this(5);
+}
+public queue(int cap) {
+	arr = new int[cap];
+}
+public int size() {
+	return size;
+}
+public boolean isFull() {
+	return size()== arr.length;
+}
+public boolean isEmpty() {
+	return size()==0;
+}
+public int peek() {
+	return arr[s];
+}
+public void add(int ali) {
+	int idx =(s+size())% arr.length;
+	arr[idx] = ali;
+	size++;
+}
+public int poll() {
+	int ans = arr[s];
+	s =(s+1)%arr.length;
+	size-- ;
+	return ans;
+}
+}

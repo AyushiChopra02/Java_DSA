71327d5a0343e2c3600352f6c6931b7a43369554
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-03-30 22:57:20 +0530

    .

--- DSA_Java/src/lec37/clieant_queue.java
+++ DSA_Java/src/lec37/clieant_queue.java
@@ -10,6 +10,6 @@ public static void main(String[] args) {
 	System.out.println(Q.poll());
 	System.out.println(Q.poll());
 	System.out.println(Q.poll()) ;
-	System.out.println(Q.poll()) ;
+	System.out.println(Q.poll());
 }
  }

7b222b5ec44101b9d19a74023d6d55dad527dbb2
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-04-03 21:58:13 +0530

    .

---
+++ DSA_Java/src/lec39/client.java
@@ -0,0 +1,40 @@
+package lec39;
+
+public class client {
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    public client() {
+        this.head = null; // Initialize head to null
+    }
+
+    public void disp() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+}

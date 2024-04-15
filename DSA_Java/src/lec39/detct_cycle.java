fad55962748240ca5b817b9f614bbfa914fcc8a6
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-04-15 20:19:08 +0530

    add

---
+++ DSA_Java/src/lec39/detct_cycle.java
@@ -0,0 +1,20 @@
+package lec39;
+
+import lec39.client.Node;
+
+public class detct_cycle {
+public static boolean main(String[] args) {
+	Node head = null;
+	Node fast = head;
+	Node slow = head;
+	while(fast != null && fast.next != null) {
+		fast = fast.next.next;
+		slow = slow.next;
+		if(fast == slow) {
+			return true;
+		}
+	}
+	return false;
+	
+}
+}

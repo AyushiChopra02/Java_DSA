1ac5b4fc376e2d040223a1ef43298a88e8a90256
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-04-16 13:10:48 +0530

    .

--- DSA_Java/src/lec39/detct_cycle.java
+++ DSA_Java/src/lec39/detct_cycle.java
@@ -17,4 +17,23 @@ public static boolean main(String[] args) {
 	return false ;
 	
 }
+public void breakCycle() {
+	Node head = null;
+	Node fast = head;
+	Node slow = head;
+	while(true) {
+		fast = fast.next.next;
+		slow = slow.next;
+		if(fast == slow) {
+			break;
+		}
+	}
+	Node special = fast;
+	Node c1 = head;
+	while(c1.next != special.next) {
+		c1 = c1.next;
+		special = special.next;
+	}
+	special.next = null;
+}
 }

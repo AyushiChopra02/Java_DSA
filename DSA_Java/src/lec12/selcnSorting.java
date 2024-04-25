720487496132359940f49b0d1c836d9349b2bd26
Ayushi <139689074+AyushiChopra02@users.noreply.github.com>
2024-04-25 22:57:21 +0530

    Update selcnSorting.java

--- DSA_Java/src/lec12/selcnSorting.java
+++ DSA_Java/src/lec12/selcnSorting.java
@@ -12,7 +12,7 @@ public static void main(String[] args) {
 public static void Selection(int arr[]) {
 	for(int last = arr.length - 1; last >=1 ; last--) {
 		int max_id =0;
-		for(int i=0 ; i <=last ; i++) {
+		for(int i=0 ; i <=last ; i++)  {
 			if (arr[i] > arr[max_id]) {
 				max_id = i;
 				

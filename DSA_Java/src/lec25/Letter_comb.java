f1a65639354ab498c0a639b21b610ca98bbf3187
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-05-09 22:59:03 +0530

    ..

--- DSA_Java/src/lec25/Letter_comb.java
+++ DSA_Java/src/lec25/Letter_comb.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 }
 public static void solve(String buttons , String ans) {
 	//BP : button: 246
-	if(buttons.isEmpty()) {
+	if(buttons.isEmpty()){
 		System.out.println(ans);
 		return ;
 		

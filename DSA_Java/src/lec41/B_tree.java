bcbe6f67ba2213ddbad1b158718981816394f09a
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-05-18 23:20:23 +0530

    .

---
+++ DSA_Java/src/lec41/B_tree.java
@@ -0,0 +1,23 @@
+package lec41;
+
+public class B_tree {
+class Node{
+	int data;
+	Node left;
+	Node right;
+	
+}
+Node root;
+public void disp() {
+	disp(root);
+}
+private void disp(Node nn) {
+	if(nn == null) {
+		return;
+	}
+	//bigger prblm 10 k
+	System.out.println(nn.data);
+	disp(nn.left);
+	disp(nn.right);
+}
+}

32c9b79aeb42a54a97ac089428517439098557db
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-05-19 19:34:16 +0530

    add

--- DSA_Java/src/lec41/B_tree.java
+++ DSA_Java/src/lec41/B_tree.java
@@ -20,4 +20,23 @@ private void disp(Node nn) {
 	disp(nn.left);
 	disp(nn.right);
 }
+	public B_tree(int[] pre, int[] in) {
+		root = createTree(pre , 0,pre.length-1,in,0,in.length-1);
+}
+	private Node createTree(int[] pre, int i, int j, int[] in, int k, int l) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+public int size() {
+	return size(root);
+}
+private int size(Node nn) {
+	if(nn == null) {
+		return 0;
+	}
+	int L= size(nn.left);
+	int R = size(nn.right);
+	return L+R+1;
+}
 }

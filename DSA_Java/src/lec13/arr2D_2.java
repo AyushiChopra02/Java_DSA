8224f8ddf8c56bc73e8dd020db2c6946bde14a93
Ayushi <139689074+AyushiChopra02@users.noreply.github.com>
2024-06-01 21:31:09 +0530

    Update arr2D_2.java

--- DSA_Java/src/lec13/arr2D_2.java
+++ DSA_Java/src/lec13/arr2D_2.java
@@ -8,7 +8,7 @@ public static void main(String[] args)  {
 	int cnt=0;
 	for(int r=0; r< mat.length; r++) {
 		for(int c = 0; c<mat[0].length ; c++) {
-	mat[r][c]= cnt;
+	mat[r][c]= cnt ;
 	cnt ++  ;
 		}
 }

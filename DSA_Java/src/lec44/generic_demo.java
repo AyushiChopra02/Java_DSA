972377aa1be2df4b25d31285cc04683c27949822
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-06-13 19:42:42 +0530

    ..

--- DSA_Java/src/lec44/generic_demo.java
+++ DSA_Java/src/lec44/generic_demo.java
@@ -1,5 +1,12 @@
 package lec44;
 
 public class generic_demo {
-
+public static void main(String[] args) {
+	student[] arr = new student[5];
+	arr[0] = new student(10, "A");
+	arr[1] = new student(2, "B");
+	arr[2] = new student(20, "Al");
+	arr[3] = new student(11, "d");
+	arr[4] = new student(1, "ds");
+}
 }

47f317e6a2ac4383e366d4f8de2b53b0c45fb276
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-06-17 19:33:38 +0530

    add

--- DSA_Java/src/lec47/HashMap.java
+++ DSA_Java/src/lec47/HashMap.java
@@ -1,5 +1,41 @@
 package lec47;
 
-public class HashMap {
+import java.util.ArrayList;
 
+public class HashMap {
+public static void main(String[] args) {
+	//HashMap<Key ,Val>
+	HashMap HM = new HashMap();
+	HM.put("A" , 10);
+	HM.put("BD" , 20);
+	HM.put("cf" , 30);
+	HM.put("dz" , 40);
+	System.out.println(HM);
+	HM.put("A", null);
+	System.out.println(HM);
+	
+	System.out.println(HM.get("A"));
+	System.out.println(HM.get("zB"));
+	
+	
+
+	System.out.println(HM.containsKey("A"));
+	
+}
+//ArrayList<String> AL_keys = new ArrayList<>(HM.keySet());
+
+private void put(String string, Object object) {
+	// TODO Auto-generated method stub
+	
+}
+
+private char[] containsKey(String string) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+private char[] get(String string) {
+	// TODO Auto-generated method stub
+	return null;
+}
 }
---
+++ DSA_Java/src/lec47/intrscn_2array.java
@@ -0,0 +1,31 @@
+package lec47;
+
+import java.util.HashMap;
+
+public class intrscn_2array {
+
+    public static void Intersecn(int[] arr1, int[] arr2) {
+        HashMap<Integer, Integer> HM = new HashMap<>();
+        
+       
+        for (int ali : arr2) {
+            int prev = HM.getOrDefault(ali, 0);
+            HM.put(ali, prev + 1);
+        }
+        
+       
+        for (int ali : arr1) {
+            if (HM.containsKey(ali) && HM.get(ali) > 0) {
+                System.out.println(ali);
+                HM.put(ali, HM.get(ali) - 1); 
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {15, 23, 4, 3, 2};
+        int[] arr2 = {4, 3, 2, 1};
+        
+        Intersecn(arr1, arr2);
+    }
+}

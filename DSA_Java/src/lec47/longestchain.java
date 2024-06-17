748b006421afeb3e6cde192a45f29b13c7cf08ac
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-06-17 19:54:48 +0530

    ..

---
+++ DSA_Java/src/lec47/longestchain.java
@@ -0,0 +1,26 @@
+package lec47;
+
+import java.util.HashSet;
+
+public class longestchain {
+private static int[] arr;
+
+public static void main(String[] args) {
+	HashSet<Integer> Set = new HashSet<>();
+	for(int ali : arr) {
+		Set.add(ali);
+	}
+	for(int ali : arr) {
+		Set.add(ali);
+		if(!Set.contains(ali-1))  {
+			//ali is the strting point of chain
+			int nn = ali;
+			while(Set.contains(nn)) {
+				System.out.println();
+				nn++;
+			}
+			System.out.println();
+		}
+	}
+}
+}

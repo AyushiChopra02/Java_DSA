e94594a16cf2a9ff002559a3a411cce67a24cb15
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-07-25 02:12:01 +0530

    ..

--- DSA_Java/src/lec53/rev_dp.java
+++ DSA_Java/src/lec53/rev_dp.java
@@ -3,18 +3,15 @@ package lec53;
 public class rev_dp {
     public static void main(String[] args) {
         int n = 10;
-        System.out.println("BU result: " + BU(n));
-        
+        System.out.println("BU result: " + BU(n)); 
         // For top-down approach with memoization
         Integer[] dp = new Integer[n + 1];
         System.out.println("Top-down result: " + fib(n, dp));
     }
-
     // Top-down approach with memoization
     public static int fib(int n, Integer[] dp) {
         if (n <= 1) {
-            return n;
-            
+            return n;      
         }
         if (dp[n] != null) {
             return dp[n];
@@ -25,6 +22,7 @@ public class rev_dp {
         return dp[n];
     }
 
+    
     // Bottom-up approach
     public static int BU(int N) {
         if (N <= 1) {

c5c82cb0d52b2e1fdb5420fe535131a6f22a598a
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-07-26 23:52:15 +0530

    ..

--- DSA_Java/src/lec54/Graph.java
+++ DSA_Java/src/lec54/Graph.java
@@ -11,34 +11,29 @@ public class Graph {
             G.put(U, new HashMap<>());
         }
     }
-
     public void signIn(int Users) {
       
         G.putIfAbsent(Users, new HashMap<>());
     }
-
     public boolean isNgh(int V1, int V2) {
         HashMap<Integer, Integer> N_V1 = G.get(V1);
         return N_V1 != null && N_V1.containsKey(V2);
     }
-
     public void addEdge(int V1, int V2, int Wt) {
         signIn(V1);
         signIn(V2);
-
         HashMap<Integer, Integer> N_V1 = G.get(V1);
         N_V1.put(V2, Wt);
 
         HashMap<Integer, Integer> N_V2 = G.get(V2);
         N_V2.put(V1, Wt);
     }
-
+    
     public void removeEdge(int V1, int V2) {
         HashMap<Integer, Integer> N_V1 = G.get(V1);
         if (N_V1 != null) {
             N_V1.remove(V2);
         }
-
         HashMap<Integer, Integer> N_V2 = G.get(V2);
         if (N_V2 != null) {
             N_V2.remove(V1);

6dd3499c34c69a3bd9bc0c79a2037e08505b4171
Ayushi <139689074+AyushiChopra02@users.noreply.github.com>
2024-08-10 21:04:31 +0530

    Update Subset.java

--- DSA_Java/src/lec10/Subset.java
+++ DSA_Java/src/lec10/Subset.java
@@ -4,7 +4,7 @@ public class Subset {
 public static void main(String[] args) {
 	int arr[] = {10,20,30,40,50} ;
 	for(int s =0 ; s < arr.length ; s++) {
-		for(int e = s  ;   e<arr.length ; e++) {
+		for(int e = s  ;    e<arr.length ; e++) {
 			for(int i=s; i<=e ; i++ ) {
 				System.out.print(arr[i] + " ");
 			}

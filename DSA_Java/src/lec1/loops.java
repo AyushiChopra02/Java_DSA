9d822aac9e5ca593a51ab9e3859f3ca396d15baf
Ayushi <139689074+AyushiChopra02@users.noreply.github.com>
2024-08-27 22:38:13 +0530

    Update loops.java

--- DSA_Java/src/lec1/loops.java
+++ DSA_Java/src/lec1/loops.java
@@ -2,7 +2,7 @@ package lec1;
 
 public class loops {
 	public static void main(String[] args) {
-		int count =1;
+		int count =1; 
 		while(count <=5) {
 			System.out.println("slapped");
 			count = count + 1;

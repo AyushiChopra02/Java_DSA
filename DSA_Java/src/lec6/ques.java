d2c5a7eb6b16ba3c7919a6ee67ac0ae3879b6d66
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-09-08 22:52:38 +0530

    .

--- DSA_Java/src/lec6/ques.java
+++ DSA_Java/src/lec6/ques.java
@@ -34,7 +34,7 @@ public class Solution {
         int[] dRow = {-1, 1, 0, 0};
         int[] dCol = {0, 0, -1, 1};
                       
-        
+         
         // BFS to calculate shortest distance from any land cell to all water cells
         while (!queue.isEmpty()) {
             int[] cell = queue.poll();

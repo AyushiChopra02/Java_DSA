f8407e7f692f3ad72f8210cd9bab5ad37e6fb664
AyushiChopra02 <ayushichopra.02@gmail.com>
2024-09-21 17:52:23 +0530

    /

--- DSA_Java/src/lec6/Solution.java
+++ DSA_Java/src/lec6/Solution.java
@@ -48,9 +48,6 @@ public class Solution {
             }
         }   
                                        
-                              
-                
-                
         int maxDist = -1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
